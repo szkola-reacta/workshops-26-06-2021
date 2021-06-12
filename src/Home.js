8ddcbfe6acf4c8b61d9f61ca2618cc6cf8733f9c
Patryk Omiotek <patryk.omiotek@gmail.com>
2021-06-12 14:01:05 +0200

    feat: passing props

--- src/Home.js
+++ src/Home.js
@@ -14,7 +14,8 @@ const listBackground = {
 
 function Offer({
   name,
-  description,
+  street,
+  city,
   country,
   cover,
   handleClick,
@@ -31,7 +32,7 @@ function Offer({
         >
           {name}
         </h2>
-        <p>{description}</p>
+        <p>{street}, {city}</p>
         <p>{country}</p>
       </div>
     </div>
@@ -43,7 +44,7 @@ Offer.propTypes = {
 }
 
 Offer.defaultProps = {
-  description: 'Brakuje opisu'
+  street: 'Brakuje nazwy ulicy'
 }
 
 // function Offer(props) {
@@ -56,6 +57,46 @@ Offer.defaultProps = {
 //     </div>
 //   );
 // }
+
+const offers = [{
+  "id": "1",
+  "name": "Six Suites, Old Town",
+  "street": "Kołodziejska 2",
+  "postal_code": "80-836",
+  "city": "Gdańsk",
+  "country_code": "PL",
+  "country_name": "Polska",
+  "cover": "https://cf.bstatic.com/xdata/images/hotel/square90/79172064.webp?k=9af8e5c75879a392d627595f8fad96d205e23ed264ec3744476b57805fa28731&o="
+} ,{
+  "id": "2",
+  "name": "Hotel Arche Geologiczna",
+  "street": "ul. Geologiczna 4",
+  "postal_code": "02-246",
+  "city": "Warszawa",
+  "country_code": "PL",
+  "country_name": "Polska",
+  "cover": "https://cf.bstatic.com/xdata/images/hotel/square90/192541890.webp?k=179376106102b1cde30c1b2fd7f220f1047d3015427af40d04e0a7bb1ecd16eb&o="
+}, {
+  "id": "3",
+  "name": "Galeria Rondo Wiatraczna Apartments",
+  "street": "Grochowska 207",
+  "postal_code": "04-077",
+  "city": "Warszawa",
+  "country_code": "PL",
+  "country_name": "Polska",
+  "cover": "https://cf.bstatic.com/xdata/images/hotel/square90/164843364.webp?k=c9b83ddbec06f9602cd3acd75506a03bc5bf250384193109fe665682f780b3ce&o="
+}, {
+  "id": "4",
+  "name": "Hotel Miodowy Młyn",
+  "street": "Legionów 3",
+  "postal_code": "27-500",
+  "city": "Opatów",
+  "country_code": "PL",
+  "country_name": "Polska",
+  "cover": "https://cf.bstatic.com/xdata/images/hotel/square90/30873029.webp?k=dea5daa25f917081221d64f1e3cb8c0d1094c40dfc00b612b508973f597aba4e&o="
+  },
+];
+
 
 function Home() {
 
@@ -81,7 +122,20 @@ function Home() {
         <h1>Wybrano: {selectedOffer}</h1>
       </div>
 
-      <Offer
+      {offers.map((elem) =>
+        <Offer
+          key={`offer-${elem.id}`}
+          // offerObj={elem}
+          name={elem.name}
+          street={elem.street}
+          city={elem.city}
+          country={elem.country}
+          cover={elem.cover}
+          handleClick={setSelectedOffer}
+        />
+      )}
+
+      {/* <Offer
         name="Six Suites, Old Town"
         description="Spokojny nocleg"
         country="Polska"
@@ -105,7 +159,7 @@ function Home() {
         country="Polska"
         cover="https://cf.bstatic.com/xdata/images/hotel/square90/30873029.webp?k=dea5daa25f917081221d64f1e3cb8c0d1094c40dfc00b612b508973f597aba4e&o="
         handleClick={setSelectedOffer}
-      />
+      /> */}
     </div>
   );
 }
