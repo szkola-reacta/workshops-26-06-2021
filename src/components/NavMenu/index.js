c56ab2208bfd5c753fa8f76167d78bd31cf42650
Patryk Omiotek <patryk.omiotek@gmail.com>
2021-06-26 13:47:35 +0200

    feat: nav menu styled

--- src/components/NavMenu/index.js
+++ src/components/NavMenu/index.js
@@ -1,15 +1,30 @@
 // import { Link } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 
+const activeStyles = {
+  color: '#c0392b',
+};
+
+const listStyle = {
+  margin: 0,
+  padding: 0,
+};
+
+const listElemStyle = {
+  display: 'inline',
+  listStyleType: 'none',
+  paddingRight: 10,
+};
+
 function NavMenu() {
   return (
     <nav>
-      <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
+      <ul style={listStyle}>
+        <li style={listElemStyle}>
+          <NavLink activeStyle={activeStyles} to="/">Home</NavLink>
         </li>
-        <li>
-          <NavLink to="/contact">Contact</NavLink>
+        <li style={listElemStyle}>
+          <NavLink activeStyle={activeStyles} to="/contact">Contact</NavLink>
         </li>
       </ul>
     </nav>
